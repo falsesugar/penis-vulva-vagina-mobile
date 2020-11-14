bb42e84d46f72c065060090edd1c06f975a6903e
Tina Rungsawang <50983382+falsesugar@users.noreply.github.com>
2020-11-14 01:39:48 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -126,267 +126,38 @@ function spawnVag8small() {
           `;
 }
 
-//med vags
-
-function spawnVag1med() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag1-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:170px;
-          height:170px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag2med() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag2-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:170px;
-          height:170px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag3med() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag3-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:170px;
-          height:170px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag4med() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag4-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:170px;
-          height:170px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag5med() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag5-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:170px;
-          height:170px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag6med() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag6-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:170px;
-          height:170px
-          z-index: 10;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag7med() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag7-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:170px;
-          height:170px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag8med() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag8-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:170px;
-          height:170px;
-          z-index: 10;" />
-          `;
-}
-
-//big vags
-
-function spawnVag1big() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag1-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:230px;
-          height:230px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag2big() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag2-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:230px;
-          height:230px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag3big() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag3-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:230px;
-          height:230px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag4big() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag4-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:230px;
-          height:230px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag5big() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag5-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:230px;
-          height:230px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag6big() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag6-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:230px;
-          height:230px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag7big() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag7-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:230px;
-          height:230px;
-          z-index: 10;" />
-          `;
-}
-
-function spawnVag8big() {
-  document.getElementById("app").innerHTML += `
-  <image crossorigin="anonymous"
-   class="draggable"
-   src="./img/vag8-alpha.png"
-   style="left:${Math.random() * window.innerWidth}px;
-          top:${Math.random() * window.innerHeight}px;
-          width:230px;
-          height:230px;
-          z-index: 10;" />
-          `;
-}
-
 //function arrays
 
-let arraySmallVag = [];
-arraySmallVag.push(spawnVag1small);
-arraySmallVag.push(spawnVag2small);
-arraySmallVag.push(spawnVag3small);
-arraySmallVag.push(spawnVag4small);
-arraySmallVag.push(spawnVag5small);
-arraySmallVag.push(spawnVag6small);
-arraySmallVag.push(spawnVag7small);
-arraySmallVag.push(spawnVag8small);
+let array1 = [];
+array1.push(spawnVag3small);
+array1.push(spawnVag6small);
 
-let arrayMedVag = [];
-arrayMedVag.push(spawnVag1med);
-arrayMedVag.push(spawnVag2med);
-arrayMedVag.push(spawnVag3med);
-arrayMedVag.push(spawnVag4med);
-arrayMedVag.push(spawnVag5med);
-arrayMedVag.push(spawnVag6med);
-arrayMedVag.push(spawnVag7med);
-arrayMedVag.push(spawnVag8med);
+let array2 = [];
+array2.push(spawnVag1small);
+array2.push(spawnVag4small);
+array2.push(spawnVag8small);
 
-let arrayBigVag = [];
-arrayBigVag.push(spawnVag1big);
-arrayBigVag.push(spawnVag2big);
-arrayBigVag.push(spawnVag3big);
-arrayBigVag.push(spawnVag4big);
-arrayBigVag.push(spawnVag5big);
-arrayBigVag.push(spawnVag6big);
-arrayBigVag.push(spawnVag7big);
-arrayBigVag.push(spawnVag8big);
+let array3 = [];
+array3.push(spawnVag2small);
+array3.push(spawnVag5small);
+array3.push(spawnVag7small);
 
 button1.addEventListener("click", function () {
-  arraySmallVag[Math.floor(Math.random() * 9)]();
+  array1[Math.floor(Math.random() * 3)]();
   description.style.visibility = "hidden";
   start.style.visibility = "hidden";
   console.log("button1 clicked");
 });
 
 button2.addEventListener("click", function () {
-  arrayMedVag[Math.floor(Math.random() * 9)]();
+  array2[Math.floor(Math.random() * 3)]();
   description.style.visibility = "hidden";
   start.style.visibility = "hidden";
   console.log("button2 clicked");
 });
 
 button3.addEventListener("click", function () {
-  arrayBigVag[Math.floor(Math.random() * 9)]();
+  array3[Math.floor(Math.random() * 3)]();
   description.style.visibility = "hidden";
   start.style.visibility = "hidden";
   console.log("button3 clicked");
